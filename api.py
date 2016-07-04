446d2b23716e480cead0e2ca5a8d8b86dad8dd5f
Alex Gajewski <agajews@gmail.com>
2016-07-04 11:37:35 -0500

    Initial commit

---
+++ api.py
@@ -0,0 +1,78 @@
+from flask import Flask
+from flask_restful import Resource, Api, reqparse
+import urllib.request
+import httplib.HTTPConnection
+import subprocess
+import os
+import time
+
+app = Flask(__name__)
+api = Api(app)
+
+parser = reqparse.RequestParser()
+parser.add_argument('furl', type=str, help='url with freezedry toml config')
+parser.add_argument('fname', type=str, help='name of freezedry toml config')
+parser.add_argument('oname', type=str, help='name for output file')
+
+# running = {
+#     'fname': None,
+#     'oname': None,
+#     'start': None,
+# }
+
+running = None
+
+
+def check_timeout():
+    global running
+    if running is not None:
+        if time.time() - running['start'] > timeout:
+            running['process'].kill()
+            running = None
+            return 'terminated'
+        return 'running'
+    return 'no process'
+
+
+class Build(Resource):
+    def put(self):
+        global running
+        if running is None:
+            args = parser.parse_args()
+            with urllib.request.urlopen(args['furl']) as response:
+                toml = response.read()
+            os.chdir(os.path.expanduser('~/apricity-build'))
+            with open('freezedry/%s' % args['fname'], 'w') as f:
+                f.write(toml)
+            cmd = ['./buildpush.sh', '-v',
+                   '-E', args['fname'],
+                   '-R', 'true',
+                   '-N', args['oname']]
+            running = {
+                'fname': args['fname'],
+                'oname': args['oname'],
+                'start': time.time(),
+                'process': subprocess.Popen(cmd)
+            }
+            return {'status': 'success'}, 201
+        return {'status': 'failure'}, 201
+
+    def get(self):
+        global running
+        if running is not None:
+            timeout = check_timeout()
+            if timeout == 'terminated':
+                return {'status': 'terminated'}, 201
+            elif timeout == 'no process':
+                return '', 501
+            desturl = '/freezedry-build/%s.iso' % \
+                running['oname']
+            conn = httplib.HTTPConnection('apricityos.com')
+            conn.request(desturl)
+            res = conn.getresponse()
+            print(res.status)
+            if res.status == 200:
+                running['process'].kill()
+                running = None
+                return {'status': 'completed'}, 201
+        return {'status': 'incompleted'}, 201
